2399894200ce97bbcfc77eb6041f100b20893ad4
wangchuang <191636716@qq.com>
2018-07-03 11:11:59 +0800

    just  do  a  favor

---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Pro on 2018/7/3.
+//  Copyright © 2018年 Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestProject
+//
+//  Created by Pro on 2018/7/3.
+//  Copyright © 2018年 Pro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
